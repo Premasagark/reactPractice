f0afce962df08c8125be69ec9fab82c4da9d5167
Premasagar k <premkm532@gmail.com>
2025-09-07 10:53:26 +0530

    React_basic1

---
+++ App.js
@@ -0,0 +1,37 @@
+
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement("div", { id: "child1" }, [
+    React.createElement(
+      "h1",
+      { id: "child1", key: "child1" },
+      "I am h1  child"
+    ),
+    React.createElement(
+      "h2",
+      { id: "child2]", key: "child2" },
+      "I am h2  child"
+    ),
+  ]),
+  React.createElement("div", { id: "child1" }, [
+    React.createElement(
+      "h1",
+      { id: "child1", key: "child1" },
+      "I am h1  child"
+    ),
+    React.createElement(
+      "h2",
+      { id: "child2]", key: "child2" },
+      "I am h2  child"
+    ),
+  ])
+);
+
+
+const heading = React.createElement("h1", {id: "heading"}, "Hello from React");
+
+console.log(heading);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
