719beb7fd497b382d30bc4eb05ba77aa77cb3ed7
Premasagar k <premkm532@gmail.com>
2025-09-08 11:21:34 +0530

    Change 2

--- App.js
+++ App.js
@@ -1,35 +1,17 @@
-
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
+const parent = React.createElement("div",{ id: "parent" },[
   React.createElement("div", { id: "child1" }, [
-    React.createElement(
-      "h1",
-      { id: "child1", key: "child1" },
-      "I am h1  child"
-    ),
-    React.createElement(
-      "h2",
-      { id: "child2]", key: "child2" },
-      "I am h2  child"
-    ),
+    React.createElement("h1",{ id: "child1", key: "child1" },"I am h1  child"),
+    React.createElement("h2",{ id: "child2", key: "child2" },"I am h2  child"),
   ]),
   React.createElement("div", { id: "child1" }, [
-    React.createElement(
-      "h1",
-      { id: "child1", key: "child1" },
-      "I am h1  child"
-    ),
-    React.createElement(
-      "h2",
-      { id: "child2]", key: "child2" },
-      "I am h2  child"
-    ),
+    React.createElement("h1",{ id: "child1", key: "child1" },"I am h1  child"),
+    React.createElement("h2",{ id: "child2", key: "child2" },"I am h2  child"),
   ])
-);
+]);
 
+console.log(parent); // return object
 
-const heading = React.createElement("h1", {id: "heading"}, "Hello from React");
+const heading = React.createElement("h1",{ id: "heading" },"Hello from React");
 
 console.log(heading);
 const root = ReactDOM.createRoot(document.getElementById("root"));
